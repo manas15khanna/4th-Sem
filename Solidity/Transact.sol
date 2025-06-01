566ce3ef16de99e58bc5c4d0b78cd18cd7a172c9
manas15khanna <manas15khanna@gmail.com>
2025-06-01 12:48:33 +0530

    Minor Changes

---
+++ Solidity/Transact.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Transact {
+    uint256 public x = 12; 
+    string public name = "Cheema Y";
+
+    function getNumber() public view returns (uint256) {
+        return x;
+    }
+
+    function getName() public view returns (string memory) {
+        return name;
+    }
+}
+
